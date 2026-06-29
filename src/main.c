b07ad8511e330d27639c2551be36ac4c62394e06
Voltarer <taran.13.official@gmail.com>
2026-06-29 11:54:56 +0000

    v2.0.3

--- src/main.c
+++ src/main.c
@@ -15,34 +15,28 @@ void init_ifTable(void);
 
 int main(int argc, char **argv) {
     // Устанавливаем (set), субагент AgentX
-    netsnmp_ds_set_boolean(NETSNMP_DS_APPLICATION_ID, NETSNMP_DS_AGENT_ROLE, 1);
+    //netsnmp_ds_set_boolean(NETSNMP_DS_APPLICATION_ID, NETSNMP_DS_AGENT_ROLE, 1);
 
-    init_master_agent();
-
-    // Инициализируем фабрику агентов под именем "test"
     init_agent("my_agent");
 
-    // Инициализируем таблицы
-    init_ifTable();
-
-    // Читаем конфигурацию "test"
     init_snmp("my_agent");
+
+    init_master_agent();
+    
+    init_ifTable();
 
     keep_running = 1;
 
-    // Перехватываем сигналы завершения, чтобы красиво закрыть агент
     signal(SIGTERM, stop_server);
     signal(SIGINT, stop_server);
 
     printf("Субагент запущен и готов к работе...\n");
     fflush(stdout); 
 
-    // Главный рабочий цикл субагента
     while (keep_running) {
         agent_check_and_process(1); 
     }
 
-    // Корректное завершение работы
     snmp_shutdown("my_agent");
 
     return 0;
